de186d1b3602b4ea88f66602024870b1628994a6
dina <dinaelhadidy966@gmail.com>
2023-09-06 18:54:17 +0300

    Update cart

--- my-app/src/component/cart-item.jsx
+++ my-app/src/component/cart-item.jsx
@@ -1,40 +1,34 @@
-import { useContext} from "react"
-import {ShopContext} from "../context/shop-context"
+import { useContext } from "react";
+import { ShopContext } from "../context/shop-context";
 
-import './cart.css'
+import "./cart.css";
 
+let CartItem = (props) => {
+  let { category, id, image, price, title } = props.data;
+  let { cartItems, addToCart, removeFromCart, updateAmount } =
+    useContext(ShopContext);
 
-
-let CartItem =(props)=>{
-    let {category ,id ,image ,price ,title} =props.data
-    let { cartItems, addToCart, removeFromCart, updateAmount} =useContext(ShopContext)
-
-
-    return(
-        <div className="cartItem">
+  return (
+    <div className="cartItem">
+      <div>
         <div>
-            <div><img src={image} alt="product image"/></div>
-            <div>{category}</div>
-            <div>{title}</div>
-            <div>Total price :$ {price * cartItems[id]}</div>
+          <img src={image} alt="product image" />
         </div>
-        <div>
-            <button onClick={()=>addToCart(id)}>+</button>
-            <input value ={cartItems[id]} onClick={(e)=>{updateAmount(Number(e.target.value),id)}}/>
-            <button onClick={()=>removeFromCart(id)}>-</button>
-        </div>
-        </div>
-
-
-
-    )
-
-
-
-   
-
-}
-
-
-
-export default CartItem
+        <div>{category}</div>
+        <div>{title}</div>
+        <div>Total price :$ {price * cartItems[id]}</div>
+      </div>
+      <div>
+        <button onClick={() => addToCart(id)}>+</button>
+        <input
+          value={cartItems[id]}
+          onClick={(e) => {
+            updateAmount(Number(e.target.value), id);
+          }}
+        />
+        <button onClick={() => removeFromCart(id)}>-</button>
+      </div>
+    </div>
+  );
+};
+export default CartItem;
--- my-app/src/component/cart.jsx
+++ my-app/src/component/cart.jsx
@@ -1,57 +1,46 @@
-import {useContext, useState } from 'react';
-import products from '../assets/products.json'
-import {ShopContext} from "../context/shop-context"
-import CartItem from './cart-item'
-import {useNavigate} from 'react-router-dom'
+import { useContext, useState } from "react";
+import products from "../assets/products.json";
+import { ShopContext } from "../context/shop-context";
+import CartItem from "./cart-item";
+import { useNavigate } from "react-router-dom";
 
+import "./cart.css";
 
-import './cart.css'
-
-
-
-
-function Cart(){   
-   /*  let [productsStored ,setProductsStored] =useState(products) */
-    let {cartItems,getTotalCartAmount,checkout} =useContext(ShopContext)
-    let totalAmount = getTotalCartAmount()
-    let navigate =useNavigate()
-    return(
-        <div className='cart'>
+function Cart() {
+  /*  let [productsStored ,setProductsStored] =useState(products) */
+  let { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  let totalAmount = getTotalCartAmount();
+  let navigate = useNavigate();
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your cart items</h1>
+        <div className="cart">
+          {products.map((product) => {
+            if (cartItems[product.id] > 0) {
+              return <CartItem data={product} />;
+            }
+          })}
+        </div>
+      </div>
+      {totalAmount > 0 ? (
         <div>
-            <h1>Your cart items</h1> 
-        <div className='cart'>
-            {products.map((product)=>{
-                if(cartItems[product.id] >0){
-                    return(
-                        <CartItem data ={product} />
-                    )
-                }
-
-            })}
-
+          <p>Total price = ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/");
+            }}
+          >
+            checkout
+          </button>
         </div>
-        </div>
-        {totalAmount >0 ? (
-            <div>
-            <p>Total price = ${totalAmount}</p>
-            <button onClick= { () => navigate('/')}>Continue Shopping</button>
-            <button onClick={()=>{checkout() ; navigate('/')}}>checkout</button>
-            </div>
-        ) : 
-        (
-            <p>Your card is empty</p>
-        )}
-
-      
-    
-        </div>
-    )
+      ) : (
+        <p>Your card is empty</p>
+      )}
+    </div>
+  );
 }
 
-
 export default Cart;
-
-
-
-
-
